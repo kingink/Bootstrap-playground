5bc1bbe64cde078d886337a7dfe5189436b9c1b2
KingInk <kfir.shay@gmail.com>
2012-12-09 23:32:06 -0500

    rename boo to main

--- boo.rb
+++
@@ -1,5 +0,0 @@
-require 'sinatra'
-
-get '/boo' do
-	"Whatever"
-end
---
+++ main.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+
+get '/' do
+	erb :index
+end
+
+get '/some_other_page' do 
+	erb :some_other_page
+end
+
+get '/boo' do
+	"Whatever"
+end
