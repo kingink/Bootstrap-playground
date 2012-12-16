2d9c0897531d0a0bb74a843f777d5a7977903d63
KingInk <kfir.shay@gmail.com>
2012-12-15 20:41:03 -0500

    make navigation bar fully work

--- main.rb
+++ main.rb
@@ -1,4 +1,23 @@
 require 'sinatra'
+require 'sinatra/content_for'
+
+helpers do
+	def nav_tab(title, url, options = {})
+		current_tab = options.delete(:current)
+		options[:class] = (current_tab == title) ? 'active' : 'inactive'
+		"<li class='#{options[:class]}'><a href='#{url}'>#{title}</a></li>"
+	end
+
+	def nav_dropdown_tab(title, options = {})
+		current_tab = options.delete(:current)
+		options[:class] = (current_tab == title) ? 'active' : 'inactive'
+		"<li class='dropdown #{options[:class]}'>"
+	end
+
+	def currently_at(tab)
+		erb :main_navigation, :locals => {current_tab: tab}
+	end	
+end
 
 get '/' do
 	erb :index
@@ -14,6 +33,10 @@ end
 
 get '/contact' do
 	erb :contact
+end
+
+get '/action' do
+	erb :action
 end
 
 get '/some_other_page' do 
