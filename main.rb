fba6e3248f296eba5bccdb27fdb63d4117ffc6c4
KingInk <kfir.shay@gmail.com>
2012-12-15 20:54:43 -0500

    eradicated tabs

--- main.rb
+++ main.rb
@@ -2,47 +2,47 @@ require 'sinatra'
 require 'sinatra/content_for'
 
 helpers do
-	def nav_tab(title, url, options = {})
-		current_tab = options.delete(:current)
-		options[:class] = (current_tab == title) ? 'active' : 'inactive'
-		"<li class='#{options[:class]}'><a href='#{url}'>#{title}</a></li>"
-	end
+  def nav_tab(title, url, options = {})
+    current_tab = options.delete(:current)
+    options[:class] = (current_tab == title) ? 'active' : 'inactive'
+    "<li class='#{options[:class]}'><a href='#{url}'>#{title}</a></li>"
+  end
 
-	def nav_dropdown_tab(title, options = {})
-		current_tab = options.delete(:current)
-		options[:class] = (current_tab == title) ? 'active' : 'inactive'
-		"<li class='dropdown #{options[:class]}'>"
-	end
+  def nav_dropdown_tab(title, options = {})
+    current_tab = options.delete(:current)
+    options[:class] = (current_tab == title) ? 'active' : 'inactive'
+    "<li class='dropdown #{options[:class]}'>"
+  end
 
-	def currently_at(tab)
-		erb :main_navigation, :locals => {current_tab: tab}
-	end	
+  def currently_at(tab)
+    erb :main_navigation, :locals => {current_tab: tab}
+  end 
 end
 
 get '/' do
-	erb :index
+  erb :index
 end
 
 get '/index' do
-	erb :index
+ erb :index
 end
 
 get '/about' do
-	erb :about
+  erb :about
 end
 
 get '/contact' do
-	erb :contact
+  erb :contact
 end
 
 get '/action' do
-	erb :action
+  erb :action
 end
 
 get '/some_other_page' do 
-	erb :some_other_page
+  erb :some_other_page
 end
 
 get '/boo' do
-	"Whatever"
+  "Whatever"
 end
